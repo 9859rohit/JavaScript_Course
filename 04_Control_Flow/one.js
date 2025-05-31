b44de3b27ca29d197d5580062c3aabcde0d19685
9859rohit <rgp4948@gmail.com>
2025-05-31 20:32:59 +0000

    One

---
+++ 04_Control_Flow/one.js
@@ -0,0 +1,51 @@
+//If Statement
+
+const temperature = 41;
+
+if(temperature===41){
+    console.log("less than 50 ");
+}
+else{
+console.log("temperature is greater than 50 ");
+}
+console.log("executed");
+
+//Conditional Operators are
+//<,>,<=,>=,==,!=,===,!==
+
+// Variable Scope
+/* const score = 200;
+
+if(score>100){
+    let power = "fly";
+    console.log(`User power: ${power}`);
+    
+}
+console.log(`User power: ${power}`); // here the program gives error it is due to variable power has only scope inside the if 
+ */
+
+//Implicit scope
+//const balance  = 1000;
+
+//if(balance >500) console.log("test implicit scope"); // implicit scope me hm ek hi line me likte h // not recommended to use
+
+//Else If 
+
+/* if(balance <500)
+{
+    console.log("less than 500 ");  
+}
+else if(balance <750){
+    console.log("less than 750 ");
+}
+else if(balance <900){
+    console.log("less than 900");  
+}
+else{
+    console.log("less than 1200");
+}
+ */
+
+// Logical Operator
+// &&, || , !
+//similar use as we used in other languages
