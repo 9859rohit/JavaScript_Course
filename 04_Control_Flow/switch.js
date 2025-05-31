60e5ff214aab3838dc880d84e8d617da537726a7
9859rohit <rgp4948@gmail.com>
2025-05-31 20:33:16 +0000

    Switch

---
+++ 04_Control_Flow/switch.js
@@ -0,0 +1,44 @@
+//Switch statement
+
+//This example is for if iur key is number;
+/* const month = 3;
+
+switch (month){
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+       
+    default:
+        console.log("default case match");
+        break;
+}
+ */
+const month = "April";
+
+switch (month){
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("February");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+       
+    default:
+        console.log("default case match");
+        break;
+}
