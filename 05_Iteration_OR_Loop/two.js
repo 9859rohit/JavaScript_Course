27edd848a8b7474fbdb425b69b7e4ae0ec8cfa9d
9859rohit <rgp4948@gmail.com>
2025-06-01 12:16:33 +0000

    while and do while loop

---
+++ 05_Iteration_OR_Loop/two.js
@@ -0,0 +1,22 @@
+
+let index = 0;
+while(index<=10){
+    console.log(`value of index in ${index}`);
+    index=index+2;
+}
+
+//While Loop 
+let myArray = ["flash","batman","superman"];
+let arrIndex = 0;
+while(arrIndex<myArray.length){
+    console.log(`value is ${myArray[arrIndex]}`);
+    arrIndex = arrIndex+1;
+}
+
+///Do while loop 
+let score = 11;
+
+do{
+    console.log(`Score value is ${score}`); 
+    score++;
+}while(score<=10);
